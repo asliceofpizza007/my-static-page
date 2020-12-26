dc5b2e928855b68f57464ce67abc7da475389499
asliceofpizza007 <a89884526@hotmail.com.tw>
2020-12-26 20:53:46 +0800

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,8 @@ import router from './router'
 import store from './store'
 import '@/assets/css/main.scss'
 import '@/assets/css/iconFonts/style.css'
+import { gsap } from 'gsap'
+import { CSSPlugin } from 'gsap/CSSPlugin'
+gsap.registerPlugin(CSSPlugin)
 
 createApp(App).use(store).use(router).mount('#app')
